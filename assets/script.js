8288204ba683ad611b7dfc0fbae0b857caad9877
Mohammed Bouali <68650187+MedBouali@users.noreply.github.com>
2025-01-03 06:12:11 +0100

    Add getHumanChoice function with input validation
    - Implemented getHumanChoice function in script.js to prompt the player for their choice (rock, paper, or scissors).
    - Added input validation to prompt the player again if the input is invalid (not one of the three valid choices).

--- assets/script.js
+++ assets/script.js
@@ -14,3 +14,19 @@ function getComputerChoice() {
 
     return compuerChoice;
 }
+
+function getHumanChoice() {
+    let humanChoice = "";
+    let choices = ["rock", "paper", "scissors"];
+    
+    do {
+        humanChoice = prompt("Please choose Rock, Paper, or Scissors:");
+        
+        if (humanChoice === null) {
+            break;
+        }
+        
+    } while (!choices.includes(humanChoice.toLocaleLowerCase()));
+    
+    return humanChoice;
+}
